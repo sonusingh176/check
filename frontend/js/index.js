d5d48da3e592a19958f4b03923ba17608d6e0442
sonusingh <sonusingh82601@gmail.com>
2023-04-10 19:34:05 +0530

    check

---
+++ frontend/js/index.js
@@ -0,0 +1,16 @@
+// import ublue from '../Assets/Blue umbrella'
+// import uyellow from '../Assets/Pink umbrella'
+// import upink from '../Assets/Yello umbrella'
+
+function handleColor(){
+    console.log(c)
+    // let imgchange=document.getElementById('u-img');
+    // if (c.value === 'blue') {
+    //     imgchange.src = ublue;
+    // } else if (c.value === 'yellow') {
+    //     imgchange.src = uyellow;
+    // } else if (c.value === 'pink') {
+    //     imgchange.src = upink;
+    // }
+
+}
